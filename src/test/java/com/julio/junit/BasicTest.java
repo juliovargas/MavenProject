05d8245444096e4be5bea8cc3debf1dc851a2841
Julio Vargas <julio.vargasp@gmail.com>
2016-03-19 22:59:40 +0100

    class Person added

--- src/test/java/com/julio/junit/BasicTest.java
+++ src/test/java/com/julio/junit/BasicTest.java
@@ -1,13 +1,20 @@
 package com.julio.junit;
 
-import static org.junit.Assert.*;
-
 import org.junit.Test;
+import static org.junit.Assert.*;
+import com.julio.classes.Person;
 
 public class BasicTest {
 
 	@Test
 	public void test() {
 		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void test2() {
+		
+		Person p1 = new Person ("pepe", 24);
+		assertEquals("pepe", p1.getName());
 	}
 }
